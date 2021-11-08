ba1c7cde79729a0fd294f781873e2edd5ede8fa9
Noé Weeks <noe.weeks@hotmail.com>
2021-11-08 14:11:24 +0100

    many changes

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,9 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+signed main(void)
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
 }
--- content/tex/preprocessor.py
+++ content/tex/preprocessor.py
@@ -146,14 +146,7 @@ def processwithcomments(caption, instream, outstream, listingslang):
         nsource = nsource.rstrip() + source[end:]
     nsource = nsource.strip()
 
-    if listingslang in ['C++', 'Java']:
-        hash_script = 'hash'
-        p = subprocess.Popen(['sh', 'content/contest/%s.sh' % hash_script], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        hsh, _ = p.communicate(nsource)
-        hsh = hsh.split(None, 1)[0]
-        hsh = hsh + ', '
-    else:
-        hsh = ''
+    hsh = ''
     # Produce output
     out = []
     if warning:
