82ea02522d8cbea1e0c4f3ac37a5f79a910dd254
Noé Weeks <noe.weeks@hotmail.com>
2022-10-11 10:15:30 +0200

    Modif book

--- content/geometry/PolygonCut.h
+++ content/geometry/PolygonCut.h
@@ -5,9 +5,8 @@
  * Source:
  * Description:\\
 \begin{minipage}{75mm}
- Returns a vector with the vertices of a polygon with everything to the left of the line going from s to e cut away.
-\end{minipage}
-\begin{minipage}{15mm}
+ Returns a vector with the vertices of a polygon with everything to the left of
+the line going from s to e cut away. \end{minipage} \begin{minipage}{15mm}
 \vspace{-6mm}
 \includegraphics[width=\textwidth]{content/geometry/PolygonCut}
 \vspace{-6mm}
@@ -23,15 +22,15 @@
 #include "lineIntersection.h"
 
 typedef Point<double> P;
-vector<P> polygonCut(const vector<P>& poly, P s, P e) {
-	vector<P> res;
-	rep(i,0,sz(poly)) {
-		P cur = poly[i], prev = i ? poly[i-1] : poly.back();
-		bool side = s.cross(e, cur) < 0;
-		if (side != (s.cross(e, prev) < 0))
-			res.push_back(lineInter(s, e, cur, prev).second);
-		if (side)
-			res.push_back(cur);
-	}
-	return res;
+vector<P> polygonCut(const vector<P> &poly, P s, P e) {
+  vector<P> res;
+  rep(i, 0, sz(poly)) {
+    P cur = poly[i], prev = i ? poly[i - 1] : poly.back();
+    bool side = s.cross(e, cur) < 0;
+    if (side != (s.cross(e, prev) < 0))
+      res.push_back(lineInter(s, e, cur, prev).second);
+    if (side)
+      res.push_back(cur);
+  }
+  return res;
 }
