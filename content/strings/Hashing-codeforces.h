99fc6f325c47c6f20bf02e76aed08c761565196e
nweeks <noe.weeks@hotmail.com>
2023-01-03 14:42:10 +0100

    Better hash

--- content/strings/Hashing-codeforces.h
+++ content/strings/Hashing-codeforces.h
@@ -8,55 +8,14 @@
  * Status: stress-tested
  */
 #pragma once
-
-typedef uint64_t ull;
-static int C; // initialized below
-
-// Arithmetic mod two primes and 2^32 simultaneously.
-// "typedef uint64_t H;" instead if Thue-Morse does not apply.
-template<int M, class B>
-struct A {
-	int x; B b; A(int x=0) : x(x), b(x) {}
-	A(int x, B b) : x(x), b(b) {}
-	A operator+(A o){int y = x+o.x; return{y - (y>=M)*M, b+o.b};}
-	A operator-(A o){int y = x-o.x; return{y + (y< 0)*M, b-o.b};}
-	A operator*(A o) { return {(int)(1LL*x*o.x % M), b*o.b}; }
-	explicit operator ull() { return x ^ (ull) b << 21; }
-};
-typedef A<1000000007, A<1000000009, unsigned>> H;
-
-struct HashInterval {
-	vector<H> ha, pw;
-	HashInterval(string& str) : ha(sz(str)+1), pw(ha) {
-		pw[0] = 1;
-		rep(i,0,sz(str))
-			ha[i+1] = ha[i] * C + str[i],
-			pw[i+1] = pw[i] * C;
-	}
-	H hashInterval(int a, int b) { // hash [a, b)
-		return ha[b] - ha[a] * pw[b - a];
-	}
-};
-
-vector<H> getHashes(string& str, int length) {
-	if (sz(str) < length) return {};
-	H h = 0, pw = 1;
-	rep(i,0,length)
-		h = h * C + str[i], pw = pw * C;
-	vector<H> ret = {h};
-	rep(i,length,sz(str)) {
-		ret.push_back(h = h * C + str[i] - pw * str[i-length]);
-	}
-	return ret;
+using ull = uint64_t;
+constexpr ull mod = (1ull << 61) - 1;
+ull modmul(ull a, ull b) {
+  ull l1 = (uint32_t)a, h1 = a >> 32, l2 = (uint32_t)b, h2 = b >> 32; 
+  ull l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2; 
+  ull ret = (l & mod) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
+  ret = (ret & mod) + (ret >> 61);
+  ret = (ret & mod) + (ret >> 61);
+  return ret - 1;
 }
-
-H hashString(string& s){H h{}; for(char c:s) h=h*C+c;return h;}
-
-#include <sys/time.h>
-int main() {
-	timeval tp;
-	gettimeofday(&tp, 0);
-	C = (int)tp.tv_usec; // (less than modulo)
-	assert((ull)(H(1)*2+1-3) == 0);
-	// ...
-}
+const int base = randint(1e11, 3e11); // pick random baseb
